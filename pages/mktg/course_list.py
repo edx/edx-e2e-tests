75fb4898ff10562a22c9d8825eee421489e4bd34
Jay Zoldak <zoldak@edx.org>
2013-11-26 15:04:59 -0500

    Fix up tests for finding courses

--- pages/mktg/course_list.py
+++ pages/mktg/course_list.py
@@ -1,8 +1,9 @@
 from e2e_framework.page_object import PageObject
+from e2e_framework.promise import EmptyPromise, fulfill_after
 from ..mktg import BASE_URL
 import re
 
-class ParseError(Exception):
+class CourseListPageError(Exception):
     """
     Could not parsing the number of results displayed
     on the course list page.
@@ -34,38 +35,45 @@ class CourseListPage(PageObject):
         return self.is_css_present('div.page-courses-all')
 
     @property
-    def num_results_shown(self):
+    def results_displayed(self):
         """
         Return a dict of the number of results displayed on the
         screen as parsed from the text shown.
         Keys are: start, end, total
         Values are of type int.
 
+        If no courses are displayed (e.g. the filter is set such that
+        no courses meet the criteria) then return None
+
         Example: "Courses: Showing 1 - 15 of 86" yields:
         {'start': 1, 'total': 86, 'end': 15}
         """
         # Get the text noting the courses shown.
-        text = self.css_text('div.view-header')[0]
+        results_text_css = 'div.view-header'
+        if self.css_count('div.view-header') == 0:
+            self.warning('No courses are currently displayed.')
+            return None
 
+        text = self.css_text(results_text_css)[0]
         parsed = re.match('^Courses:\s*Showing (\d+) - (\d+) of (\d+)\s*$', text)
-        if (parsed is not None) and (len(parsed.groups()) == 3):
 
+        if (parsed is not None) and (len(parsed.groups()) == 3):
             return dict(zip(['start', 'end', 'total'], map(int, parsed.groups())))
 
         else:
             msg = 'Cannot parse the results displayed from the string: {0}'.format(
                 text
             )
-            raise ParseError(msg)
+            raise CourseListPageError(msg)
 
     @property
-    def course_title_list(self):
+    def course_titles(self):
         """
         Retrieve the list of available course IDs on the page.
         """
         return self.css_text('h2.course-title')
 
-    def press_pager_link(self, text):
+    def show_results(self, text):
         """
         Press the pager link to display different courses in the list.
         For the text, use either the number of the page, or one of
@@ -76,4 +84,9 @@ class CourseListPage(PageObject):
         else:
             title = 'Go to page {0}'.format(text)
 
-        self.css_click('a[title="{0}"]'.format(title))
+        pagination_promise = EmptyPromise(
+            lambda: self.is_browser_on_page,
+            'redrew the page'
+        )
+        with fulfill_after(pagination_promise):
+            self.css_click('a[title="{0}"]'.format(title))
--- test_mktg/test_find_courses.py
+++ test_mktg/test_find_courses.py
@@ -4,6 +4,7 @@ End to end tests for finding courses
 from e2e_framework.web_app_test import WebAppTest
 from pages.mktg.course_list import CourseListPage
 from nose.tools import assert_equal, assert_not_equal
+from nose.plugins.skip import SkipTest
 
 
 class FindCoursesTest(WebAppTest):
@@ -22,7 +23,7 @@ class FindCoursesTest(WebAppTest):
         self.ui.visit('mktg.course_list')
 
         # Grab the courses shown and total count
-        displayed = self.ui['mktg.course_list'].num_results_shown
+        displayed = self.ui['mktg.course_list'].results_displayed
 
         # By default we should only show the first 15 results
         assert_equal(displayed['start'], 1)
@@ -41,24 +42,25 @@ class FindCoursesTest(WebAppTest):
         self.ui.visit('mktg.course_list')
 
         # Grab the courses shown and total count
-        orig_num = self.ui['mktg.course_list'].num_results_shown
+        orig_num = self.ui['mktg.course_list'].results_displayed
         # Grab the titles of the courses being displayed
-        orig_titles = self.ui['mktg.course_list'].course_title_list
+        orig_titles = self.ui['mktg.course_list'].course_titles
 
         # There should be more than 15 courses in most environments,
         # if there isn't this test would not be applicable
         if (orig_num['total'] < 16):
-            pass
+            msg = 'Skipping pagination test because there are not enough courses'
+            raise SkipTest(msg)
 
         # press the next link
-        self.ui['mktg.course_list'].press_pager_link('next')
+        self.ui['mktg.course_list'].show_results('next')
 
         # Verify that the results have incremented
-        new_num = self.ui['mktg.course_list'].num_results_shown
+        new_num = self.ui['mktg.course_list'].results_displayed
         assert_equal(new_num['start'], orig_num['start']+15)
 
         # Verify that the courses displayed have changed
-        new_titles = self.ui['mktg.course_list'].course_title_list
+        new_titles = self.ui['mktg.course_list'].course_titles
         assert_not_equal(new_titles, orig_titles)
 
 
@@ -68,23 +70,24 @@ class FindCoursesTest(WebAppTest):
         self.ui.visit('mktg.course_list')
 
         # Grab the courses shown and total count
-        orig_num = self.ui['mktg.course_list'].num_results_shown
+        orig_num = self.ui['mktg.course_list'].results_displayed
         # Grab the titles of the courses being displayed
-        orig_titles = self.ui['mktg.course_list'].course_title_list
+        orig_titles = self.ui['mktg.course_list'].course_titles
 
         # There should be more than 15 courses in most environments,
         # if there isn't this test would not be applicable
         if (orig_num['total'] < 16):
-            pass
+            msg = 'Skipping pagination test because there are not enough courses'
+            raise SkipTest(msg)
 
         # press the next link
-        self.ui['mktg.course_list'].press_pager_link('last')
+        self.ui['mktg.course_list'].show_results('last')
 
         # Verify that the results have incremented
-        new_num = self.ui['mktg.course_list'].num_results_shown
+        new_num = self.ui['mktg.course_list'].results_displayed
         assert_equal(new_num['end'], orig_num['total'])
 
         # Verify that the courses displayed have changed
-        new_titles = self.ui['mktg.course_list'].course_title_list
+        new_titles = self.ui['mktg.course_list'].course_titles
         assert_not_equal(new_titles, orig_titles)
 
