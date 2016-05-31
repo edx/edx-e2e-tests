a5a5496fe3a2ddb37163ab8404a0d2240a818584
raeeschachar <raees.chachar@arbisoft.com>
2016-05-31 12:59:47 +0000

    Updated test_demo file

--- regression/tests/lms/test_demo.py
+++ regression/tests/lms/test_demo.py
@@ -26,5 +26,5 @@ class DemoTest(WebAppTest):
         """
         self.login_page.visit()
         self.login_page.login(self.DEMO_COURSE_USER, self.DEMO_COURSE_PASSWORD)
-        self.assertEquals(self.login_page.q(css='.wrapper-header-courses .header-courses').text[0].lower(),
-                          'my courses', 'Log-in successful')
+        self.assertEqual(self.login_page.q(css='.wrapper-header-courses .header-courses').text[0].lower(),
+                          'my courses', msg='User not logged in as expected.')
