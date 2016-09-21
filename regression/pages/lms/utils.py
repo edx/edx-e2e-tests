e9c23810fc8c1f61f948df478db8a5d68791c36e
raeeschachar <raees.chachar@arbisoft.com>
2016-09-21 09:04:37 +0000

    Test for Analytics link on Instructor Dashboard

---
+++ regression/pages/lms/utils.py
@@ -0,0 +1,19 @@
+"""
+Utility functions for lms page objects.
+"""
+from opaque_keys.edx.locator import CourseLocator
+
+
+def get_course_key(course_info, module_store='split'):
+    """
+    Returns the course key based upon course info passed.
+    Course key depends upon the module store passed. The default
+    module store is 'split'.
+    """
+    course_key = CourseLocator(
+        course_info['org'],
+        course_info['number'],
+        course_info['run'],
+        deprecated=(module_store == 'draft')
+    )
+    return unicode(course_key)
---
+++ regression/tests/lms/test_instructor_dashboard.py
@@ -0,0 +1,47 @@
+"""
+End to end tests for Instructor Dashboard.
+"""
+from bok_choy.web_app_test import WebAppTest
+from regression.pages.lms.login_lms import LmsLogin
+from regression.pages.lms.dashboard_lms import DashboardPageExtended
+from regression.pages.lms.instructor_dashboard import (
+    InstructorDashboardPageExtended
+)
+from regression.pages.lms.course_page_lms import CourseInfoPageExtended
+from regression.tests.helpers import LoginHelper, get_course_info
+from regression.pages.lms.utils import get_course_key
+
+
+class AnalyticsTest(WebAppTest):
+    """
+    Regression tests on Analytics on Instructor Dashboard
+    """
+
+    def setUp(self):
+        super(AnalyticsTest, self).setUp()
+        self.login_page = LmsLogin(self.browser)
+        LoginHelper.login(self.login_page)
+        course_info = get_course_info()
+        self.dashboard_page = DashboardPageExtended(self.browser)
+        self.instructor_dashboard = InstructorDashboardPageExtended(
+            self.browser,
+            get_course_key(course_info)
+        )
+        self.course_page = CourseInfoPageExtended(
+            self.browser,
+            get_course_key(course_info)
+        )
+
+        self.dashboard_page.select_course('Manual Smoke Test Course 1 - Auto')
+        self.course_page.wait_for_page()
+        self.instructor_dashboard.visit()
+
+    def test_analytics_link(self):
+        """
+        Verifies that edX Insights link is clicked and displayed
+        """
+        self.instructor_dashboard.click_analytics_tab()
+        self.assertEquals(
+            self.instructor_dashboard.click_edx_insights(),
+            'INSIGHTS'
+        )
