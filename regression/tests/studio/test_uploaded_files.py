8e5e34ade7f0ad192412f939ea739f0edcee2a80
raeeschachar <raees.chachar@arbisoft.com>
2016-11-30 12:18:35 +0000

    Pagination test on files and uploads page

--- regression/tests/studio/test_uploaded_files.py
+++ regression/tests/studio/test_uploaded_files.py
@@ -1,12 +1,15 @@
 """
 Test uploaded files.
 """
+import os
+from shutil import copyfile
 from regression.tests.studio.studio_base_test import StudioBaseTestClass
 from regression.pages.studio.utils import upload_new_file
 from regression.pages.studio.login_studio import StudioLogin
 from regression.tests.helpers import LoginHelper, get_course_info
 
 from regression.pages.studio.asset_index_studio import AssetIndexPageExtended
+from regression.pages import UPLOAD_FILE_DIR
 
 
 class UploadedFileTest(StudioBaseTestClass):
@@ -28,7 +31,7 @@ class UploadedFileTest(StudioBaseTestClass):
         self.asset_page.visit()
         self.file_names = [
             'README.rst',
-            'Image.png'
+            'test_pdf.pdf'
         ]
 
     def upload_files(self):
@@ -82,9 +85,55 @@ class UploadedFileTest(StudioBaseTestClass):
         Then file should be deleted and no longer available.
         """
         self.upload_files()
-        # Index of files to be deleted.
-        file_index_to_delete = [0, 1]
-        # Delete the files.
-        self.asset_page.delete_file(file_index_to_delete)
-        # Assert files has been deleted.
-        self.assertNotIn(self.file_names, self.asset_page.get_file_names())
+        file_names = self.asset_page.get_file_names()
+        for name in file_names:
+            self.asset_page.click_delete_file()
+            # Assert files have been deleted.
+            self.assertNotIn(name, self.asset_page.get_file_names())
+
+
+class UploadedFilePaginationTest(StudioBaseTestClass):
+    """
+    Test uploaded files.
+    """
+    def setUp(self):
+        super(UploadedFilePaginationTest, self).setUp()
+        self.login_page = StudioLogin(self.browser)
+        LoginHelper.login(self.login_page)
+        self.course_info = get_course_info()
+
+        self.asset_page = AssetIndexPageExtended(
+            self.browser,
+            self.course_info['org'],
+            self.course_info['number'],
+            self.course_info['run']
+        )
+        self.asset_page.visit()
+
+    def test_pagination(self):
+        """
+        Verifies that user can successfully navigate between multiple pages
+        """
+        file_name = '{}.png'
+        file_names = ['1.png']
+        for num in range(2, 52):
+            file_names.append(file_name.format(num))
+            copyfile(
+                UPLOAD_FILE_DIR + '/' + '1.png',
+                UPLOAD_FILE_DIR + '/' + file_name.format(num)
+            )
+
+        # Open file upload prompt.
+        self.asset_page.open_upload_file_prompt()
+        # Upload the files.
+        upload_new_file(self.asset_page, file_names)
+        # Assert that pages are now 2 in total.
+        self.assertEqual('1', self.asset_page.get_page_count())
+        self.asset_page.click_next_page_link()
+        # Assert that pages are now 2 in total.
+        self.assertEqual('2', self.asset_page.get_page_count())
+
+        file_names.pop(0)
+        # Remove files from directory
+        for file_name in file_names:
+            os.remove(UPLOAD_FILE_DIR + '/' + file_name)
