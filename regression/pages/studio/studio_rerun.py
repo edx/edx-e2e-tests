0a5eddb40770f2c3bf14acd034e0b45fb4184acd
raeeschachar <raees.chachar@arbisoft.com>
2017-01-09 11:18:06 +0000

    Addressed comments

--- regression/pages/studio/studio_rerun.py
+++ regression/pages/studio/studio_rerun.py
@@ -5,13 +5,16 @@ import os
 from uuid import uuid4
 from bok_choy.page_object import PageObject
 from regression.pages.studio import BASE_URL
+from regression.tests.helpers import get_course_info
+from regression.pages.lms.utils import get_course_key
 
 
 class StudioRerun(PageObject):
     """
     Studio Course Rerun for Manual Smoke Test Course
     """
-    url = BASE_URL + '/course_rerun/course-v1:ArbiRaees+AR-1000+fall'
+    course_details = get_course_key(get_course_info())
+    url = BASE_URL + '/course_rerun/' + course_details
 
     def is_browser_on_page(self):
         return self.q(css='.rerun-course-save.is-disabled').present
