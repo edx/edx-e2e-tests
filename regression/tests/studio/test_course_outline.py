e6751945d74c49f8e3494f58caced9cf21777146
Jay Zoldak <jz@C02RD1YPFVH8.tld>
2017-01-23 14:21:43 -0500

    Rewrite course outline test as a flow and optimize

--- regression/tests/studio/test_course_outline.py
+++ regression/tests/studio/test_course_outline.py
@@ -9,74 +9,70 @@ from regression.pages.studio.course_outline_page import (
     CourseOutlinePageExtended
 )
 from regression.pages.studio.login_studio import StudioLogin
-from regression.pages.studio.studio_home import DashboardPageExtended
 from regression.pages.studio.settings_studio import SettingsPageExtended
+from regression.pages.studio.studio_home import DashboardPageExtended
 from regression.tests.helpers import (
     get_course_info, get_course_display_name
 )
 
+DEMO_COURSE_USER = os.environ.get('USER_LOGIN_EMAIL')
+DEMO_COURSE_PASSWORD = os.environ.get('USER_LOGIN_PASSWORD')
+
 
 class StudioCourseOutlineTest(WebAppTest):
-    """
-    Test for navigating to the Studio Home page
-    """
+    """Tests of the Course Outline in Studio."""
 
-    DEMO_COURSE_USER = os.environ.get('USER_LOGIN_EMAIL')
-    DEMO_COURSE_PASSWORD = os.environ.get('USER_LOGIN_PASSWORD')
-
-    def setUp(self):
-        """
-        Initialize the page object
-        """
-        super(StudioCourseOutlineTest, self).setUp()
-        self.studio_login_page = StudioLogin(self.browser)
-        self.studio_home_page = DashboardPageExtended(self.browser)
-        self.studio_login_page.visit()
-        self.studio_login_page.login(self.DEMO_COURSE_USER,
-                                     self.DEMO_COURSE_PASSWORD)
-
-        self.course_info = get_course_info()
-
-        self.studio_course_outline = CourseOutlinePageExtended(
-            self.browser, self.course_info['org'], self.course_info['number'],
-            self.course_info['run'])
-
-        self.schedule_page = SettingsPageExtended(
-            self.browser, self.course_info['org'], self.course_info['number'],
-            self.course_info['run'])
-
-        self.studio_home_page.visit()
-        self.studio_home_page.select_course(get_course_display_name())
-        self.studio_course_outline.wait_for_page()
-
-    def test_edit_start_button(self):
+    def test_course_outline(self):
         """
         Verifies that user can click Edit Start Date button and is navigated
-        to Schedule and Details page
+        to Schedule and Details page, and that the Help link for
+        'Learn more about content visibility settings' is working.
         """
-        self.studio_course_outline.click_edit_start_date_button()
-        self.schedule_page.wait_for_page()
+        studio_login_page = StudioLogin(self.browser)
+        studio_home_page = DashboardPageExtended(self.browser)
+        studio_login_page.visit()
+        studio_login_page.login(DEMO_COURSE_USER, DEMO_COURSE_PASSWORD)
 
-    def test_content_visibility_help(self):
-        """
-        Scenario: Help link for
-        'Learn more about content visibility settings' is working
-        Given that I am on the 'Course Outline' page.
-        And I want help about the content visibility settings.
-        And I click 'Learn more about content visibility settings'
-        in the sidebar
-        Then Help link should open.
-        And url should point to the content visibility help page.
-        """
-        # The url we want to see in anchor help element.
+        course_info = get_course_info()
+
+        studio_course_outline = CourseOutlinePageExtended(
+            self.browser, course_info['org'], course_info['number'],
+            course_info['run'])
+
+        schedule_page = SettingsPageExtended(
+            self.browser, course_info['org'], course_info['number'],
+            course_info['run'])
+
+        # Verification only, should be on this page after login.
+        studio_home_page.wait_for_page()
+
+        # Navigate to the course's outline page
+        studio_home_page.select_course(get_course_display_name())
+        studio_course_outline.wait_for_page()
+
+        # First verify the Help link
         expected_href = 'https://edx.readthedocs.io/projects/' \
                         'edx-partner-course-staff/en/latest/' \
                         'developing_course/controlling_content_visibility.html'
         # Assert that help link is correct.
         assert_side_bar_help_link(
             test=self,
-            page=self.studio_course_outline,
+            page=studio_course_outline,
             href=expected_href,
             help_text='Learn more about content visibility settings',
             as_list_item=False
         )
+
+        # If the help page is still up (see LT-53), then close it.
+        if self.browser.current_url.startswith('https://edx.readthedocs.io'):
+            # TODO wrap this in a try/except block or otherwise harden,
+            # make sure that you now have an active window (the other one)
+            # and it's the right one (i.e. Studio or LMS)
+            self.browser.close()  # close only the current window
+            self.browser.switch_to_window(self.browser.window_handles[0])
+
+        # Now do the verification for the edit start date button.
+        studio_course_outline.click_edit_start_date_button()
+
+        # This wait_for_page will also assert that we are on the correct page.
+        schedule_page.wait_for_page()
