abad940d53c143b6fa172e05be4718d41deebfde
Kashif Chaudhry <kchaudhry@edx.org>
2017-01-26 19:36:56 +0500

    Update white_label.sh

--- jenkins/white_label.sh
+++ jenkins/white_label.sh
@@ -19,7 +19,7 @@ pip install -r requirements/base.txt
 
 
 # Run the tests
-organizations="MITProfessionalX HarvardMedGlobalAcademy HarvardXPLUS"
+organizations="HarvardXPLUS"
 
 # Run General tests on all organizations using Chrome
 for organization in ${organizations}; do
