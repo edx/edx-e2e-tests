8d7745cf30a775b1a06e8c3892f82bd6e7fc86ab
Kashif Chaudhry <kchaudhry@edx.org>
2017-02-15 15:53:29 +0500

    ecommerce pages

---
+++ regression/pages/ecommerce/coupon_const.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+Constant used in coupon tests
+"""
+from datetime import datetime, timedelta
+
+from regression.pages.whitelabel.const import ORG
+
+# Coupon Error messages on basket page
+
+EXPIRED_CODE_ERROR = "Coupon code '{}' has expired."
+
+FUTURE_CODE_ERROR = "Coupon code '{}' has expired."
+
+SINGLE_USE_CODE_REUSE_ERROR = "Coupon code '{}' has already been redeemed."
+
+ONCE_PER_CUSTOMER_CODE_MAX_LIMIT = \
+    'Your basket does not qualify for a coupon code discount.'
+
+ONCE_PER_CUSTOMER_CODE_SAME_USER_REUSE = "Coupon code '{}' is invalid."
+
+INVALID_DOMAIN_ERROR_MESSAGE_ON_BASKET = \
+    'Your basket does not qualify for a coupon code discount.'
+
+# Coupon Error messages on redeem url page
+
+EXPIRED_REDEEM_URL_ERROR = 'This coupon code has expired.'
+
+FUTURE_REDEEM_URL_ERROR = 'This coupon code is not yet valid.'
+
+SINGLE_USE_REDEEM_URL_REUSE_ERROR = 'This coupon has already been used'
+
+ONCE_PER_CUSTOMER_REDEEM_URL_MAX_LIMIT = \
+    'This coupon code is no longer available.'
+
+ONCE_PER_CUSTOMER_REDEEM_URL_SAME_USER_REUSE = \
+    'You have already used this coupon in a previous order'
+
+INVALID_DOMAIN_ERROR_MESSAGE_ON_REDEEM_URL = \
+    'You are not eligible to use this coupon.'
+
+INACTIVE_ACCOUNT_ERROR_MESSAGE = \
+    'You need to activate your account in order to redeem this coupon.'
+
+# Coupons info
+
+DEFAULT_START_DATE = (datetime.today() -
+                      timedelta(days=15)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+EXPIRED_END_DATE = (datetime.today() -
+                    timedelta(days=5)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+DEFAULT_END_DATE = (datetime.today() +
+                    timedelta(days=15)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+FUTURE_START_DATE = (datetime.today() +
+                     timedelta(days=5)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+COURSE_CATALOG_TYPE = {'single': 'Single course', 'multi': 'Multiple courses'}
+
+SEAT_TYPE = {'prof': 'professional', 'ver': 'verified'}
+
+COURSE_SEAT_TYPES = {'prof': ['professional']}
+
+COUPON_TYPE = {'disc': 'Discount code', 'enroll': 'Enrollment code'}
+
+BENEFIT_TYPE = {'abs': 'Absolute', 'per': 'Percentage'}
+
+VOUCHER_TYPE = {
+    'single': 'Single use',
+    'once_per_cust': 'Once per customer',
+    'multi': 'Multi-use'
+}
+
+BENEFIT_VALUE = {'fixed': 60, 'per': 40}
+
+STOCK_RECORD_IDS = {
+    'HarvardMedGlobalAcademy': [8398],
+    'HarvardXPLUS': [8396],
+    'MITProfessionalX': [8400],
+}
+
+STOCK_RECORD_ID = STOCK_RECORD_IDS[ORG]
+
+# Coupon users
+
+COUPON_USERS = {
+    'coupon_user_01': 'wl_coupon_user01@example.com',
+    'coupon_user_02': 'wl_coupon_user02@example.com',
+    'coupon_user_03': 'wl_coupon_user03@example.com'
+}
+
+VALID_DOMAIN_USERS = {
+    'coupon_user_04': 'wl_coupon_user04@emaildomainfour.com',
+    'coupon_user_05': 'wl_coupon_user05@emaildomainfive.com'
+}
+
+INVALID_DOMAIN_USERS = {
+    'coupon_user_06': 'wl_coupon_user06@emaildomainsix.com',
+    'coupon_user_07': 'wl_coupon_user07@emaildomainseven.com'
+}
+
+# Email domains
+
+VALID_EMAIL_DOMAINS = "emaildomainfour.com,emaildomainfive.com"
+
+# Courses for dynamic coupons testing
+COUPON_COURSES = {
+    u'HarvardMedGlobalAcademy': {
+        u'course-v1:HarvardMedGlobalAcademy+HMGA01+2016': {
+            'price': 100.0, 'title': u'Automated Tests-HMGA01'
+        },
+        u'course-v1:HarvardMedGlobalAcademy+HMGA02+2016': {
+            'price': 100.0, 'title': u'Automated Tests-HMGA02'
+        },
+        u'course-v1:HarvardMedGlobalAcademy+HMGA03+2016': {
+            'price': 200.0, 'title': u'Automated Tests-HMGA03'
+        },
+        u'course-v1:HarvardMedGlobalAcademy+HMGA04+2016': {
+            'price': 200.0, 'title': u'Automated Tests-HMGA04'
+        },
+        u'course-v1:HarvardMedGlobalAcademy+HMGA05+2016': {
+            'price': 300.0, 'title': u'Automated Tests-HMGA05'
+        },
+        u'course-v1:HarvardMedGlobalAcademy+HMGA06+2016': {
+            'price': 300.0, 'title': u'Automated Tests-HMGA06'
+        }
+    },
+    u'MITProfessionalX': {
+        u'course-v1:MITProfessionalX+MITPX01+2016': {
+            'price': 100.0, 'title': u'Automated Tests-MITPX01'
+        },
+        u'course-v1:MITProfessionalX+MITPX02+2016': {
+            'price': 100.0, 'title': u'Automated Tests-MITPX02'
+        },
+        u'course-v1:MITProfessionalX+MITPX03+2016': {
+            'price': 200.0, 'title': u'Automated Tests-MITPX03'
+        },
+        u'course-v1:MITProfessionalX+MITPX04+2016': {
+            'price': 200.0, 'title': u'Automated Tests-MITPX04'
+        },
+        u'course-v1:MITProfessionalX+MITPX05+2016': {
+            'price': 300.0, 'title': u'Automated Tests-MITPX05'
+        },
+        u'course-v1:MITProfessionalX+MITPX06+2016': {
+            'price': 300.0, 'title': u'Automated Tests-MITPX06'
+        }
+    },
+    u'HarvardXPLUS': {
+        u'course-v1:HarvardXPLUS+HXP01+2016': {
+            'price': 100.0, 'title': u'Automated Tests-HXP01'
+        },
+        u'course-v1:HarvardXPLUS+HXP02+2016': {
+            'price': 100.0, 'title': u'Automated Tests-HXP02'
+        },
+        u'course-v1:HarvardXPLUS+HXP03+2016': {
+            'price': 200.0, 'title': u'Automated Tests-HXP03'
+        },
+        u'course-v1:HarvardXPLUS+HXP04+2016': {
+            'price': 200.0, 'title': u'Automated Tests-HXP04'
+        },
+        u'course-v1:HarvardXPLUS+HXP05+2016': {
+            'price': 300.0, 'title': u'Automated Tests-HXP05'
+        },
+        u'course-v1:HarvardXPLUS+HXP06+2016': {
+            'price': 300.0, 'title': u'Automated Tests-HXP06'
+        }
+    }
+}
+
+COURSES_CATALOG = COUPON_COURSES[ORG]
+
+COURSES_CATALOG_QUERIES = {
+    'MITProfessionalX': 'org:MITProfessionalX and number:MITPX*',
+    'HarvardXPLUS': 'org:HarvardXPLUS and number:HXP*',
+    'HarvardMedGlobalAcademy': 'org:HarvardMedGlobalAcademy and number:HMGA*'
+}
+
+CATALOG_QUERY = COURSES_CATALOG_QUERIES[ORG]
+
---
+++ regression/pages/ecommerce/receipt_page.py
@@ -0,0 +1,155 @@
+"""
+Receipt page
+"""
+import time
+
+from bok_choy.page_object import PageObject
+
+from regression.pages.common.utils import (
+    convert_date_format,
+    extract_numerical_value_from_price_string
+)
+from regression.pages.whitelabel.const import (
+    TIME_OUT_LIMIT,
+    WAIT_TIME,
+    INITIAL_WAIT_TIME
+)
+from regression.pages.whitelabel.dashboard_page import DashboardPage
+
+
+class ReceiptException(Exception):
+    """
+    Catch failures in receipt page
+    """
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ReceiptPage(PageObject):
+    """
+    Receipt
+    """
+
+    url = None
+
+    def is_browser_on_page(self):
+        """
+        Verifies that receipt report is present on the page:
+        """
+        return self.q(css='.container').present
+
+    def is_receipt_displayed(self):
+        """
+        This is a temporary work around due to the fact that sometimes the
+        receipt pages shows and error, however on refreshing the page it
+        starts showing original receipt
+        Returns:
+            True if receipt is found:
+        """
+        # self.wait_for_element_visibility(
+        #     '#receipt-container',
+        #     'wait for receipt to become visible'
+        # )
+        found = False
+        t_end = time.time() + TIME_OUT_LIMIT
+        # Run the loop for a pre defined time
+        current_url = self.browser.current_url
+        while time.time() < t_end:
+            time.sleep(INITIAL_WAIT_TIME)
+            try:
+                if not self.q(
+                        css='.next.action-primary.right[href="/dashboard"]'
+                        # css='#receipt-container'
+                ).is_present():
+                    raise ReceiptException
+                found = True
+                break
+            except ReceiptException:
+                time.sleep(WAIT_TIME)
+                self.browser.get(current_url)
+                self.wait_for_page()
+        if found:
+            return True
+
+    @property
+    def order_num(self):
+        """
+        Get the order number from receipt
+        Returns:
+            order number:
+        """
+        return self.q(
+            css='.report.report-receipt>tbody>tr>td:nth-of-type(1)').text[0]
+            # css='.order-summary>dl>dd:nth-of-type(1)').text[0]
+
+    @property
+    def order_desc(self):
+        """
+        Get the order description from receipt
+        Returns:
+            order description:
+        """
+        return self.q(css='.report.report-receipt>tbody>tr>td:nth-of-type(2)').text[0]
+        # return self.q(css='.course-description>span').text[0]
+
+    @property
+    def order_date(self):
+        """
+        Get the order date from receipt
+        Raises:
+            order date:
+        """
+        date_string = self.q(
+            css='.report.report-receipt>tbody>tr>td:nth-of-type(3)'
+            # xpath=".//*[@id='receipt-container']//dt[text()='Order Date:']"
+            #       "/following-sibling::dd"
+        ).text[0]
+        return convert_date_format(
+            date_string,
+            '%Y-%m-%dT%H:%M:%SZ',
+            # '%B %d, %Y',
+            '%Y-%m-%d'
+        )
+
+    @property
+    def order_amount(self):
+        """
+        Get the order amount from receipt
+        Returns:
+            order amount:
+        """
+        amount = self.q(
+            css='.report.report-receipt>tbody>tr>td:nth-of-type(4)'
+            # css='.line-price.price'
+        ).text[0]
+        return extract_numerical_value_from_price_string(amount)
+
+    @property
+    def total_amount(self):
+        """
+        Get the total amount
+        Returns:
+            total amount:
+        """
+        total_amount = self.q(
+            css='.report.report-receipt>tfoot>tr>td>.value-amount').text[0]
+            # css='.order-total:nth-of-type(2)>.price').text[0]
+        return extract_numerical_value_from_price_string(total_amount)
+
+    @property
+    def billed_to(self):
+        """
+        Get the information about the person to whom it is billed
+        Returns:
+            billed_to_info:
+        """
+        return self.q(css='.confirm-message>a').text[0]
+
+    def go_to_dashboard(self):
+        """
+        Go to user dashboard page by clicking on Go to Dashboard button
+        """
+        self.q(css='.next.action-primary.right[href="/dashboard"]').click()
+        # self.q(css='.dashboard-link.nav-link').click()
+        DashboardPage(self.browser).wait_for_page()
