cd5b0b988fc05e5d3e712552695b3281b7d0ff56
1991Muhu <muddasser1991@yahoo.com>
2017-03-09 11:53:15 +0000

    Update course test

---
+++ regression/pages/studio/import_course_page.py
@@ -0,0 +1,36 @@
+"""
+Import course page
+"""
+from edxapp_acceptance.pages.studio.import_export import (
+    ImportMixin
+)
+from edxapp_acceptance.pages.common.utils import click_css
+
+from regression.pages.studio.course_page_studio import CoursePageExtended
+from regression.pages import UPLOAD_FILE_DIR
+
+
+class ImportCoursePageExtended(ImportMixin, CoursePageExtended):
+    """
+    Extended course import page.
+    """
+    def upload_tarball(self, tarball_filename):
+        """
+        Upload a tarball to be imported.
+        """
+        asset_file_path = UPLOAD_FILE_DIR + "/" + tarball_filename
+        # Make the upload elements visible to the WebDriver.
+        self.browser.execute_script(
+            '$(".file-name-block").show();$(".file-input").show()'
+        )
+        # Upload the file.
+        self.q(css='input[type="file"]')[0].send_keys(asset_file_path)
+        # Upload the same file again. Reason behind this is to decrease the
+        # probability or fraction of times the failure occur. Please be
+        # noted this doesn't eradicate the root cause of the error, it
+        # just decreases to failure rate to minimal.
+        # Jira ticket reference: TNL-4191.
+        self.q(css='input[type="file"]')[0].send_keys(asset_file_path)
+        # Some of the tests need these lines to pass so don't remove them.
+        self._wait_for_button()
+        click_css(self, '.submit-button', require_notification=False)
---
+++ regression/tests/studio/test_course_import.py
@@ -0,0 +1,56 @@
+"""
+Course import test
+"""
+from bok_choy.web_app_test import WebAppTest
+
+from regression.pages.studio.import_course_page import ImportCoursePageExtended
+from regression.pages.studio.course_outline_page import (
+    CourseOutlinePageExtended
+)
+from regression.tests.helpers.utils import get_course_info
+from regression.tests.helpers.api_clients import StudioLoginApi
+
+
+class TestCourseImport(WebAppTest):
+    """
+    Tests the Course import page
+    """
+    tarball_name = 'course.tar.gz'
+    import_page_class = ImportCoursePageExtended
+    landing_page_class = CourseOutlinePageExtended
+
+    def setUp(self):
+        super(TestCourseImport, self).setUp()
+        login_api = StudioLoginApi()
+        login_api.authenticate(self.browser)
+
+        self.course_info = get_course_info()
+
+        self.import_page = ImportCoursePageExtended(*self.page_args())
+        self.course_outline_page = CourseOutlinePageExtended(*self.page_args())
+
+    def page_args(self):
+        """
+        Common arguments for pages to be used in tests.
+        """
+        return [
+            self.browser,
+            self.course_info['org'],
+            self.course_info['number'],
+            self.course_info['run']
+        ]
+
+    def test_course_updated(self):
+        """
+        Tests that we can update(import) course using tarball.
+        """
+        self.import_page.visit()
+        self.import_page.upload_tarball(self.tarball_name)
+        self.import_page.wait_for_upload()
+        self.course_outline_page.visit()
+        # There's a section named
+        # 'Section :754c5e889ac3489e9947ba62b916bdab' in the tarball.
+        self.assertIn(
+            "Section :754c5e889ac3489e9947ba62b916bdab",
+            self.course_outline_page.get_section_names()
+        )
