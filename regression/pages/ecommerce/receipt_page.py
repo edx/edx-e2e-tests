7803ac0fc0289e8ab10d6312a725a756f577b87f
Kashif Chaudhry <kchaudhry@edx.org>
2017-03-17 11:13:17 +0500

    receipt

--- regression/pages/ecommerce/receipt_page.py
+++ regression/pages/ecommerce/receipt_page.py
@@ -47,30 +47,30 @@ class ReceiptPage(PageObject):
         Returns:
             True if receipt is found:
         """
-        # self.wait_for_element_visibility(
-        #     '#receipt-container',
-        #     'wait for receipt to become visible'
-        # )
-        found = False
-        t_end = time.time() + TIME_OUT_LIMIT
-        # Run the loop for a pre defined time
-        current_url = self.browser.current_url
-        while time.time() < t_end:
-            time.sleep(INITIAL_WAIT_TIME)
-            try:
-                if not self.q(
-                        css='.next.action-primary.right[href="/dashboard"]'
-                        # css='#receipt-container'
-                ).is_present():
-                    raise ReceiptException
-                found = True
-                break
-            except ReceiptException:
-                time.sleep(WAIT_TIME)
-                self.browser.get(current_url)
-                self.wait_for_page()
-        if found:
-            return True
+        self.wait_for_element_visibility(
+            '#receipt-container',
+            'wait for receipt to become visible'
+        )
+        # found = False
+        # t_end = time.time() + TIME_OUT_LIMIT
+        # # Run the loop for a pre defined time
+        # current_url = self.browser.current_url
+        # while time.time() < t_end:
+        #     time.sleep(INITIAL_WAIT_TIME)
+        #     try:
+        #         if not self.q(
+        #                 css='.next.action-primary.right[href="/dashboard"]'
+        #                 # css='#receipt-container'
+        #         ).is_present():
+        #             raise ReceiptException
+        #         found = True
+        #         break
+        #     except ReceiptException:
+        #         time.sleep(WAIT_TIME)
+        #         self.browser.get(current_url)
+        #         self.wait_for_page()
+        # if found:
+        #     return True
 
     @property
     def order_num(self):
@@ -80,8 +80,8 @@ class ReceiptPage(PageObject):
             order number:
         """
         return self.q(
-            css='.report.report-receipt>tbody>tr>td:nth-of-type(1)').text[0]
-            # css='.order-summary>dl>dd:nth-of-type(1)').text[0]
+            # css='.report.report-receipt>tbody>tr>td:nth-of-type(1)').text[0]
+            css='.order-summary>dl>dd:nth-of-type(1)').text[0]
 
     @property
     def order_desc(self):
@@ -90,8 +90,8 @@ class ReceiptPage(PageObject):
         Returns:
             order description:
         """
-        return self.q(css='.report.report-receipt>tbody>tr>td:nth-of-type(2)').text[0]
-        # return self.q(css='.course-description>span').text[0]
+        # return self.q(css='.report.report-receipt>tbody>tr>td:nth-of-type(2)').text[0]
+        return self.q(css='.course-description>span').text[0]
 
     @property
     def order_date(self):
@@ -101,15 +101,15 @@ class ReceiptPage(PageObject):
             order date:
         """
         date_string = self.q(
-            css='.report.report-receipt>tbody>tr>td:nth-of-type(3)'
-            # xpath=".//*[@id='receipt-container']//dt[text()='Order Date:']"
-            #       "/following-sibling::dd"
+            # css='.report.report-receipt>tbody>tr>td:nth-of-type(3)'
+            xpath=".//*[@id='receipt-container']//dt[text()='Order Date:']"
+                  "/following-sibling::dd"
         ).text[0]
         return convert_date_format(
             date_string,
             '%Y-%m-%dT%H:%M:%SZ',
-            # '%B %d, %Y',
-            '%Y-%m-%d'
+            '%B %d, %Y',
+            # '%Y-%m-%d'
         )
 
     @property
@@ -120,8 +120,8 @@ class ReceiptPage(PageObject):
             order amount:
         """
         amount = self.q(
-            css='.report.report-receipt>tbody>tr>td:nth-of-type(4)'
-            # css='.line-price.price'
+            # css='.report.report-receipt>tbody>tr>td:nth-of-type(4)'
+            css='.line-price.price'
         ).text[0]
         return extract_numerical_value_from_price_string(amount)
 
@@ -133,8 +133,8 @@ class ReceiptPage(PageObject):
             total amount:
         """
         total_amount = self.q(
-            css='.report.report-receipt>tfoot>tr>td>.value-amount').text[0]
-            # css='.order-total:nth-of-type(2)>.price').text[0]
+            # css='.report.report-receipt>tfoot>tr>td>.value-amount').text[0]
+            css='.order-total:nth-of-type(2)>.price').text[0]
         return extract_numerical_value_from_price_string(total_amount)
 
     @property
@@ -150,6 +150,6 @@ class ReceiptPage(PageObject):
         """
         Go to user dashboard page by clicking on Go to Dashboard button
         """
-        self.q(css='.next.action-primary.right[href="/dashboard"]').click()
-        # self.q(css='.dashboard-link.nav-link').click()
+        # self.q(css='.next.action-primary.right[href="/dashboard"]').click()
+        self.q(css='.dashboard-link.nav-link').click()
         DashboardPage(self.browser).wait_for_page()
