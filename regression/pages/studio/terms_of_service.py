fd9f4b6b18c80db4ac86172d6e71e8bc0bb28e28
raeeschachar <raees.chachar@arbisoft.com>
2018-02-16 18:40:34 +0500

    Revert "Remove flaky StudioFooterTest."

---
+++ regression/pages/studio/terms_of_service.py
@@ -0,0 +1,17 @@
+"""
+Terms of Service page
+"""
+from bok_choy.page_object import PageObject
+from regression.pages.studio import LOGIN_BASE_URL
+
+
+class TermsOfService(PageObject):
+    """
+    Terms of Service page
+    """
+    url = LOGIN_BASE_URL + '/edx-terms-service'
+
+    def is_browser_on_page(self):
+        return "Please read these Terms of Service" in self.q(
+            css='.content-section'
+        ).text[0]
