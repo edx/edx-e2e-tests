5971dc7063f0a83077255212a2cd4e1f43712427
Awais Jibran <awaisdar001@gmail.com>
2018-07-24 12:40:23 +0500

    Revert "EDUCATOR-2335: Remove flaky test_assignment_types test."

--- regression/tests/studio/test_studio_grading.py
+++ regression/tests/studio/test_studio_grading.py
@@ -1,6 +1,8 @@
 """
 Test studio grading
 """
+from uuid import uuid4
+
 from bok_choy.web_app_test import WebAppTest
 from regression.pages.studio.grading_studio import GradingPageExtended
 from regression.pages.studio.course_outline_page import (
@@ -62,3 +64,55 @@ class StudioGradingTest(WebAppTest):
         self.grading_page.visit()
         self.assertEquals(
             self.grading_page.letter_grade('.letter-grade'), 'Pass')
+
+    def test_assignment_types(self):
+        """
+        Verifies that user can add/delete assignment types
+        """
+        # Delete any existing assignment types
+        self.grading_page.delete_all_assignment_types()
+
+        self.grading_page.add_new_assignment_type()
+        self.grading_page.fill_assignment_type_fields(
+            name='Final',
+            abbreviation='Finale',
+            total_grade='100',
+            total_number='2',
+            drop='1'
+        )
+        self.assertEquals(
+            self.grading_page.assignment_name_field_value(), ['Final'])
+
+        # Navigating to course outline page to see if the added assignment
+        # is available to use on subsections
+        self.studio_course_outline.visit()
+
+        section_name = 'Section :{}'.format(uuid4().hex)
+        self.studio_course_outline.add_section_with_name(section_name)
+        self.assertIn(
+            section_name,
+            self.studio_course_outline.q(
+                css='.incontext-editor-value').text
+        )
+
+        subsection_name = 'Subsection :{}'.format(uuid4().hex)
+        self.studio_course_outline.add_subsection_with_name(
+            subsection_name)
+        self.assertIn(
+            subsection_name,
+            self.studio_course_outline.q(
+                css='.incontext-editor-value').text
+        )
+
+        self.studio_course_outline.open_subsection_settings_dialog()
+        self.assertIn(
+            'Final',
+            self.studio_course_outline.get_subsection_grade()
+        )
+
+        # Delete added section
+        self.studio_course_outline.cancel_subsection_settings()
+        self.studio_course_outline.delete_section()
+        # Delete added assignment type
+        self.grading_page.visit()
+        self.grading_page.delete_assignment_type()
