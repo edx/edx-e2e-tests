2ff7df414137f646994bd4d219ed77abd4227ef3
Nimisha Asthagiri <nasthagiri@edx.org>
2019-01-03 09:31:28 -0500

    Revert "Fix and un-skip Studio login/logout tests"

--- regression/pages/studio/login_studio.py
+++ regression/pages/studio/login_studio.py
@@ -2,17 +2,11 @@
 Studio login page
 """
 from edxapp_acceptance.pages.studio.login import LoginPage
-from regression.pages.studio import LMS_LOGIN_BASE_URL
+from regression.pages.studio import LOGIN_BASE_URL
 
 
 class StudioLogin(LoginPage):
     """
     This class is an extended class of LoginPage.
     """
-    url = LMS_LOGIN_BASE_URL + '/login'
-
-    def is_browser_on_page(self):
-        """
-        Verifies if the browser is on the correct page
-        """
-        return self.q(css='.js-login.login-button').visible
+    url = LOGIN_BASE_URL + '/signin'
