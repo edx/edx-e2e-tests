20ccaee9ab510fb429436eb0d3ebbc1e7714caf9
Matthew Piatetsky <mpiatetsky@edx.org>
2019-03-13 13:57:14 -0400

    upload file as part of test

--- regression/tests/lms/test_graded_problem.py
+++ regression/tests/lms/test_graded_problem.py
@@ -6,6 +6,7 @@ from bok_choy.web_app_test import WebAppTest
 from regression.pages.lms.dashboard_lms import DashboardPageExtended
 from regression.pages.lms.course_page_lms import CourseHomePageExtended
 from regression.pages.lms.lms_courseware import CoursewarePageExtended
+from regression.pages.studio.import_course_page import ImportCoursePageExtended
 from regression.tests.helpers.utils import (
     get_course_info, get_course_display_name
 )
@@ -17,6 +18,7 @@ class GradedProblemTest(WebAppTest):
     """
     Regression tests on submitting a graded problem
     """
+    tarball_name = 'course.tar.gz'
 
     def setUp(self):
         super(GradedProblemTest, self).setUp()
@@ -24,19 +26,37 @@ class GradedProblemTest(WebAppTest):
         login_api = LmsLoginApi()
         login_api.authenticate(self.browser)
 
-        course_info = get_course_info()
+        self.course_info = get_course_info()
+
+        self.import_page = ImportCoursePageExtended(*self.page_args())
+        self.import_page.visit()
+        self.import_page.upload_tarball(self.tarball_name)
+        self.import_page.wait_for_upload()
+
         self.dashboard_page = DashboardPageExtended(self.browser)
         self.course_page = CourseHomePageExtended(
             self.browser,
-            get_course_key(course_info)
+            get_course_key(self.course_info)
         )
         self.lms_courseware = CoursewarePageExtended(
             self.browser,
-            get_course_key(course_info)
+            get_course_key(self.course_info)
         )
+
         self.dashboard_page.visit()
         self.dashboard_page.select_course(get_course_display_name())
         self.course_page.wait_for_page()
+
+    def page_args(self):
+        """
+        Common arguments for pages to be used in tests.
+        """
+        return [
+            self.browser,
+            self.course_info['org'],
+            self.course_info['number'],
+            self.course_info['run']
+        ]
 
     def test_graded_problem(self):
         """
