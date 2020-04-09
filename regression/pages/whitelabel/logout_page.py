fc3aed0399aa1d28bc6f1d0f1f19eff125b06444
Feanil Patel <feanil@edx.org>
2020-04-09 11:54:54 -0400

    we're on the right page.

--- regression/pages/whitelabel/logout_page.py
+++ regression/pages/whitelabel/logout_page.py
@@ -4,7 +4,6 @@ Logout Page
 from __future__ import absolute_import
 
 import os
-
 from bok_choy.page_object import PageObject
 
 from regression.pages.whitelabel import ECOM_URL_WITH_AUTH
@@ -16,6 +15,14 @@ class EcommerceLogoutPage(PageObject):
     """
 
     url = os.path.join(ECOM_URL_WITH_AUTH, 'logout/')
+
+    def is_browser_on_page(self):
+        """
+        Is browser on the page?
+        Returns:
+            True if user drop down is visible on the page:
+        """
+        return True
 
     def logout_from_ecommerce(self):
         """
