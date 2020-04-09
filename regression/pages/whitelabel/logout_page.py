84c1eff95cc874aeb528d0ec15c19d76b6e7ab64
Feanil Patel <feanil@edx.org>
2020-04-09 12:32:40 -0400

    All page objects need this method.

--- regression/pages/whitelabel/logout_page.py
+++ regression/pages/whitelabel/logout_page.py
@@ -15,3 +15,11 @@ class EcommerceLogoutPage(PageObject):
     """
 
     url = os.path.join(ECOM_URL_WITH_AUTH, 'logout/')
+
+    def is_browser_on_page(self):
+        """
+        Is browser on the page?
+        Returns:
+            True if the sign out message is on the page.
+        """
+        return "you have signed out" in self.browser.page_source.lower()
