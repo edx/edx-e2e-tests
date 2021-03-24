49662881dea4801bc24368e055c7d7a28001d57e
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-03-24 13:52:03 +0500

    Revert "temp: Fixing a flaky test."

--- regression/tests/lms/test_graded_problem.py
+++ regression/tests/lms/test_graded_problem.py
@@ -1,6 +1,7 @@
 """
 End to end tests for submitting a graded problem.
 """
+from unittest import skip
 
 from bok_choy.web_app_test import WebAppTest
 
@@ -53,10 +54,11 @@ class GradedProblemTest(WebAppTest):
             self.course_info['run']
         ]
 
+    @skip("BOM-2460: Test is failing on Ubuntu 20.04.")
     def test_graded_problem(self):
         """
         Verifies submission of a graded problem
         """
         self.course_page.click_resume_button()
-        self.lms_courseware.visit()
+        self.lms_courseware.wait_for_page()
         self.lms_courseware.submit_graded_problem()
