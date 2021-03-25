f6a115b27ce3b157b1c5012bb39ae257f29f009c
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-03-25 16:17:06 +0500

    debugging

--- regression/tests/common/test_html_component.py
+++ regression/tests/common/test_html_component.py
@@ -77,15 +77,14 @@ class StudioViewTest(StudioLmsComponentBaseTest):
         # self.lms_courseware.go_to_section(section_name, subsection_name)
         # View unit in the studio
         self.lms_courseware.view_unit_in_studio()
-        assert 1==2
 
         # self.unit_container_page.wait_for_page(timeout=30)
         #
         # # # Correct unit component should open.
-        # self.assertEqual(
-        #     get_data_locator(self.unit_container_page),
-        #     data_locator, 'Correct component is opened'
-        # )
+        self.assertEqual(
+            get_data_locator(self.unit_container_page),
+            data_locator, 'Correct component is opened'
+        )
         # # Remove the added section
         # self.studio_course_outline.visit()
         # self.studio_course_outline.delete_section()
